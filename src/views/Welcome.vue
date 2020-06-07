192f940db456f5a6e36137442b742dad5bfba245
Ashanti Haring <ashanti.haring@gmail.com>
2020-06-07 22:36:45 +0200

    add awesome

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,23 @@
-<template >
-  <v-container class="text-center overflow">
-    <v-row class="mt-12">
-      <v-col cols="12" class="py-0">
-        <h1 class="custom-title">
+<template>
+  <v-container>
+    <v-row class="mt-12 text-center ">
+      <v-col cols="12" class="py-0 ">
+        <h2 class="primary--text pretitle">AWESOME</h2>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <h1 cols="12" class="custom-title">
           <span class="party">PARTY</span>
           <span class="wall">WALL</span>
         </h1>
       </v-col>
-      <v-col cols="12" lg="8" offset-lg="2" class="py-0">
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="8" offset-lg="2" class="py-0 px-6">
         <h2 class="subtitle">
-          No hustle, just enjoy. Let us do take care about food and drinks and you care about your guests. Explore food and drinks or
-          sell your awesome stuff.
+          No hustle, just enjoy. Let us do take care about food and drinks and you care about your guests. Explore food and drinks
+          or sell your awesome stuff.
         </h2>
       </v-col>
     </v-row>
@@ -32,7 +39,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col class="text-center">
         <v-btn color="accent" class="py-6 px-8 mt-12" to="/register">
           Explore Now
           <v-icon class="ml-3">mdi-party-popper</v-icon>
@@ -98,7 +105,6 @@
     bottom: 0;
   }
   .custom-title {
-    margin-top: 73px;
     font-size: 6rem;
   }
 }
